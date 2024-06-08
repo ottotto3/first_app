8614e460c1f262a5ab23f19a3833270d77214e00
ottotto3 <xufangyi8@gmail.com>
2024-06-09 00:09:08 +0900

    [Add]新規投稿

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists' => 'lists#create'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  get 'top' => 'homes#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,6 +13,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
-  get 'top' => 'homes#top'
 end
